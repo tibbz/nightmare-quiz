50a8ffc6ac381e536540c14f28ee89da5e73baf9
Tibbers <sarahgaco@gmail.com>
2022-04-29 11:44:27 +0200

    Added Q&A

--- quiz/js/data.js
+++ quiz/js/data.js
@@ -1,26 +1,30 @@
 var jsonData = [
     {
-        "q" : "",
-        "opt2": "",
-        "opt3": "",
-        "answer": ""
+        "q" : "What's the name of A Nightmare on Elm Street 1 director?",
+        "opt1": "Robert Englund",
+        "opt2": "Wes Craven",
+        "opt3": "John Saxon",
+        "answer": "Wes Craven"
     },
     {
-        "q" : "",
-        "opt2": "",
-        "opt3": "",
-        "answer": ""
+        "q" : "What's the name of Freddy Krueger's mom?",
+        "opt1": "Amanda",
+        "opt2": "Alice",
+        "opt3": "Kirsten",
+        "answer": "Amanda"
     },
     {
-        "q" : "",
-        "opt2": "",
-        "opt3": "",
-        "answer": ""
+        "q" : "What's the name of A Nightmare on Elm Street's first death ever?",
+        "opt1": "Jesse Walsh",
+        "opt2": "Nancy Thompson",
+        "opt3": "Tina Gray",
+        "answer": "Tina Gray"
     },
     {
-        "q" : "",
-        "opt2": "",
-        "opt3": "",
-        "answer": ""
+        "q" : "How did Tina die",
+        "opt1": "Stabbed in the side with bladed glove, thrown, impaled on car's tail fin",
+        "opt2": "Slashed across chest with bladed glove, levitated, thrown, dragged up wall to ceiling, dropped",
+        "opt3": "Pulled into waterbed, stabbed in torso with bladed glove, drowned",
+        "answer": "Slashed across chest with bladed glove, levitated, thrown, dragged up wall to ceiling, dropped"
     }
 ];
